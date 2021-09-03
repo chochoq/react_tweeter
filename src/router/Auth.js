685772331371596ab2b2f4ecdf9135760bfd764d
chochoq <aubepe@gmail.com>
2021-09-04 03:54:58 +0900

    (add) github login

--- src/router/Auth.js
+++ src/router/Auth.js
@@ -48,7 +48,7 @@ const Auth = () => {
         if (name === "google") {
             provider = new firebaseInstance.auth.GoogleAuthProvider();
         } else if (name === "github") {
-            provider = new firebaseInstance.auth.GoogleAuthProvider();
+            provider = new firebaseInstance.auth.GithubAuthProvider();
         }
         const data = await authService.signInWithPopup(provider);
         console.log(data);
