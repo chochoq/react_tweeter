b192e3991c9fe8eaac12b0303abff6e5f8fd3eb6
chochoq <aubepe@gmail.com>
2021-09-03 20:48:26 +0900

    (add) firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase/app";
+
+
+const firebaseConfig = {
+    
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
