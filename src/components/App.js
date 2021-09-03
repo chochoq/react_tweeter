5018574a414c28da080e4caf111c5270b1003e42
chochoq <aubepe@gmail.com>
2021-09-04 01:39:44 +0900

    (add) login

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
-import React,{ useState } from 'react';
+import React,{ useState, useEffect } from 'react';
 import AppRouter from 'components/AppRouter';
 import { authService } from 'fbase';
 
 function App() {
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  // 저장되어있을 경우 자동로그인
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    })
+}, []);
+  
   return (
     <div className="App">
-      <AppRouter isLoggedIn={isLoggedIn}/>
+      {init ? <AppRouter isLoggedIn={isLoggedIn}/>: "init.....g..."}
     </div>
   );
 }
--- src/router/Auth.js
+++ src/router/Auth.js
@@ -9,6 +9,8 @@ const Auth = () => {
 
     const [newAccount, setNewAccount] = useState(true);
 
+    const [error, setError] = useState("");
+
     const onChange = (e) => {
         const { target: { name, value } } = e;
         if (name === "email") {
@@ -37,8 +39,12 @@ const Auth = () => {
             }
             console.log(data);
         } catch (err) {
-            console.log(err);
+            setError(err.message);
         }
+    };
+
+    const toggleAccount = (e) => {
+        setNewAccount((prev) => !prev);
     };
 
     return (
@@ -48,8 +54,11 @@ const Auth = () => {
             <input name="email" type="email" placeholder="이메일을 적어주세요" required value={email} onChange={onChange}/>
             <input name="password" type="password" placeholder="비밀번호를 적어주세요" required value={password} onChange={onChange}/>
             <input type="submit" value={newAccount ? "Create Account" : "Login"} />
+            {error}
         </form>
+            <span onClick={toggleAccount}>{newAccount? "log in": "ceate A"}</span>
         <div>
+            
             <button>구글로그인</button>
             <button>깃허브로그인</button>
         </div>
