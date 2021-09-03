c7a74af5d54e9f5def1f4073fa7705e556bae6bb
chochoq <aubepe@gmail.com>
2021-09-04 00:55:37 +0900

    (add) Create Account fb

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React,{ useState } from 'react';
 import AppRouter from 'components/AppRouter';
 import { authService } from 'fbase';
 
-console.log(authService.currentUser)
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
--- src/router/Auth.js
+++ src/router/Auth.js
@@ -1,31 +1,53 @@
-import React,{ useState }  from 'react';
+import React, { useState } from 'react';
+import { authService } from 'fbase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const [newAccount, setNewAccount] = useState(true);
+
     const onChange = (e) => {
         const { target: { name, value } } = e;
         if (name === "email") {
             setEmail(value);
-            console.log(email);
         } else if (name === "password") {
             setPassword(value);
         }
     };
 
-    const onSubmit = (e) => {
+    const onSubmit = async(e) => {
         e.preventDefault();
+        try {
+            let data;
+            if (newAccount) {
+                // Create Account
+                data = await createUserWithEmailAndPassword(
+                    email,
+                    password
+                );
+            } else {
+                // Login
+                data = await signInWithEmailAndPassword(
+                    email,
+                    password
+                );
+            }
+            console.log(data);
+        } catch (err) {
+            console.log(err);
+        }
     };
 
     return (
     <div>
         <form onSubmit={onSubmit}>
             <h1>login</h1>
-            <input name="email" type="text" placeholder="이메일을 적어주세요" required value={email} onChange={onChange}/>
+            <input name="email" type="email" placeholder="이메일을 적어주세요" required value={email} onChange={onChange}/>
             <input name="password" type="password" placeholder="비밀번호를 적어주세요" required value={password} onChange={onChange}/>
-            <input type="submit" value="Login" />
+            <input type="submit" value={newAccount ? "Create Account" : "Login"} />
         </form>
         <div>
             <button>구글로그인</button>
